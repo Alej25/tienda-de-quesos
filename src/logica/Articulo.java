0350dbcf100c0ecabc397e7e8d35691b58336725
Alej25 <85592431+Alej25@users.noreply.github.com>
2023-03-21 22:00:28 -0600

    principal proyecto

---
+++ src/logica/Articulo.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+/**
+ *
+ * @author HP
+ */
+public class Articulo {
+    private String Nombre;
+    private int Cantidad;
+    private double precio;
+    private double subtotal;
+    
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(int Cantidad) {
+        this.Cantidad = Cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal() {
+        this.subtotal = (this.Cantidad * this.precio);
+    }
+    
+    
+}
